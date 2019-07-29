d3ddbd0b6da525800ac3f32b7baf7136ed6ac45f
Ryan Supelana <ryanchester98@gmail.com>
2019-07-29 14:14:17 +0800

    Add primary map functions

--- src/app/livemap/livemap.component.ts
+++ src/app/livemap/livemap.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { MouseEvent } from "@agm/core";
 
 @Component({
   selector: "app-livemap",
@@ -8,8 +9,32 @@ import { Component, OnInit } from "@angular/core";
 export class LivemapComponent implements OnInit {
   lat: number = 14.5409;
   lng: number = 121.0503;
+  zoom: number = 16;
+
+  markers: marker[] = [];
 
   constructor() {}
 
   ngOnInit() {}
+
+  onMapClick($event: MouseEvent) {
+    console.log("map clicked");
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: true
+    });
+  }
+
+  onMarkerClick(index: number) {
+    console.log("marker deleted");
+    this.markers.splice(index, 1);
+  }
+}
+
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
 }
