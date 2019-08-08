087ed77f6a55465a7a75c62d5758ddef9b74f341
Ryan Supelana <ryanchester98@gmail.com>
2019-08-08 09:54:41 +0800

    Fix error upon log in

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,31 +1,46 @@
-import { Component, OnInit, AfterViewChecked, OnDestroy } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ChangeDetectorRef,
+  AfterViewChecked,
+  OnDestroy
+} from "@angular/core";
 import { User } from "../user";
-
+import { Router } from "@angular/router";
 import { CookieService } from "ngx-cookie-service";
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.css"]
 })
-export class NavbarComponent implements OnInit, AfterViewChecked {
+export class NavbarComponent implements OnInit, AfterViewChecked, OnDestroy {
   currentUser: User = undefined;
-  constructor(private cookieService: CookieService) {}
+  constructor(
+    private cookieService: CookieService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     if (this.cookieService.get("currentUser"))
       this.currentUser = JSON.parse(this.cookieService.get("currentUser"));
-    // console.log(this.currentUser);
   }
 
   ngAfterViewChecked() {
     if (this.cookieService.get("currentUser"))
       this.currentUser = JSON.parse(this.cookieService.get("currentUser"));
-    // console.log(this.currentUser);
+    this.cdr.detectChanges();
+  }
+
+  ngOnDestroy() {
+    this.cookieService.delete("currentUser");
+    this.currentUser = undefined;
   }
 
   onLogout() {
     this.cookieService.delete("currentUser");
     this.currentUser = undefined;
     console.log("Logged out");
+    this.router.navigate(["/"]);
   }
 }
